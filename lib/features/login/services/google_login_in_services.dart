517576ed800f4fea672883fe5a5de5ab74266a81
sabi1125 <sabir_barahi4@hotmail.com>
2026-03-15 17:17:59 +0900

    add: finished implementing google sign in

--- lib/features/login/services/google_login_in_services.dart
+++ lib/features/login/services/google_login_in_services.dart
@@ -1,20 +1,26 @@
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:mapminder_mobile/core/security_store.dart';
+import 'package:mapminder_mobile/features/login/repository/google_login_repository.dart';
+import 'package:mapminder_mobile/features/login/services/login_services.dart';
 
-class GoogleLoginInServices {
-  static GoogleLoginInServices? _instance;
+class GoogleLoginServices implements LoginServices {
+  static GoogleLoginServices? _instance;
 
-  factory GoogleLoginInServices() {
-    _instance ??= GoogleLoginInServices._();
+  factory GoogleLoginServices() {
+    _instance ??= GoogleLoginServices._();
     return _instance!;
   }
 
   final GoogleSignIn signIn = GoogleSignIn.instance;
-  GoogleLoginInServices._() {
+  GoogleLoginServices._() {
     signIn.initialize();
   }
+  final googleLoginRepository = GoogleLoginRepository();
+  final securityStorage = SecurityStore();
 
   // signInWithGoogle() get google's idToken that is required to register or create JWT token
-  Future<String> signInWithGoogle() async {
+  @override
+  Future<void> login() async {
     try {
       final GoogleSignInAccount account = await signIn.authenticate();
       final auth = account.authentication;
@@ -22,7 +28,8 @@ class GoogleLoginInServices {
       if (idToken == null) {
         throw Exception("Failed to authenticate user");
       }
-      return idToken;
+      final jwtToken = await googleLoginRepository.login(idToken);
+      await securityStorage.writeData("jwt_token", jwtToken);
     } on GoogleSignInException catch (e) {
       throw Exception("error occurred while login with google. ${e.code}");
     } catch (error)  {
