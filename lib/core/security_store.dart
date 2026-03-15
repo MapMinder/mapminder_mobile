8c3e81b5dcbf70e64bc927122e4bbd91423193f4
sabi1125 <sabir_barahi4@hotmail.com>
2026-03-15 17:14:46 +0900

    add: added httpclient and secure storage for jwt

---
+++ lib/core/security_store.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecurityStore {
+  static SecurityStore? _instance;
+
+  factory SecurityStore() {
+    _instance ??= SecurityStore._();
+    return _instance!;
+  }
+
+  SecurityStore._();
+  final storage = FlutterSecureStorage();
+
+  Future<void> writeData(String key, String value) async {
+    await storage.write(key: key, value: value);
+  }
+
+  Future<String?> readData(String key) async {
+    String? value = await storage.read(key: key);
+    return value;
+  }
+}
