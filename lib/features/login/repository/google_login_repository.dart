c3a9cf209744f42f2a4f418341642a9cff9865dc
sabi1125 <sabir_barahi4@hotmail.com>
2026-03-21 16:19:22 +0900

    update: added getUserInfo and seperated concerns

--- lib/features/login/repository/google_login_repository.dart
+++ lib/features/login/repository/google_login_repository.dart
@@ -1,9 +1,10 @@
 import 'package:dio/dio.dart';
 import 'package:mapminder_mobile/core/http_client.dart';
 import 'package:mapminder_mobile/features/login/dto/login_dto.dart';
-import 'package:mapminder_mobile/features/login/repository/login_repository.dart';
+import 'package:mapminder_mobile/features/login/repository/interfaces/oauth_login_repository.dart';
+import 'package:mapminder_mobile/features/login/domain/user.dart';
 
-class GoogleLoginRepository implements LoginRepository {
+class GoogleLoginRepository implements OauthLoginRepository {
   static GoogleLoginRepository? _instance;
 
   factory GoogleLoginRepository() {
@@ -26,6 +27,19 @@ class GoogleLoginRepository implements LoginRepository {
     } on DioException catch (e) {
       throw Exception("error occurred while calling the backend. ${e.error}");
     } catch (error) {
+      throw Exception("Unexpected error occurred");
+    }
+  }
+
+  @override
+  Future<User> getUserInfo() async {
+    try {
+      Response response = await client.dio.get("/user/me");
+      User user = User.fromJson(response.data["result"]);
+      return user;
+    } on DioException catch (e) {
+      throw Exception("error occurred while calling the backend. ${e.error}");
+    } catch (error){
       throw Exception("Unexpected error occurred");
     }
   }
